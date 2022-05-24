0316b31cda719f07c48850a0f482539b41fc1693
Nelly <NellyisDev@gmail.com>
2022-05-24 14:17:41 -0400

    trying to fix CSS pages

---
+++ src/javascript/script.js
@@ -0,0 +1,12 @@
+// CREATING A DARK THEME FOR WEBSITE
+let icon = document.getElementById('icon');
+
+// WHEN ICON CLICKED TOGGLE CLASS AND CHANGE ICON
+icon.onclick = () => {
+  document.body.classList.toggle('dark-theme');
+  if (document.body.classList.contains('dark-theme')) {
+    icon.src = './src/img/moon.png';
+  } else {
+    icon.src = './src/img/sun.png';
+  }
+};
