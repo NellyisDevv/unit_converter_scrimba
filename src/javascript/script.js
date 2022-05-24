bce3e862ac40bcf4166aab4bdf587cdf79aee1f8
Nelly <NellyisDev@gmail.com>
2022-05-24 18:31:33 -0400

    Javascript Meter/Feet Conversion started.

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -10,3 +10,34 @@ icon.onclick = () => {
     icon.src = '../src/img/sun.png';
   }
 };
+
+// PROMPT USER TO ENTER A NUMBER FOR CONVERSION
+let countEl = document.getElementById('count-el');
+// EXECUTION OF METERS TO FEET
+let firstP = document.getElementById('first-p');
+// PLACEHOLDER NUMBERS
+let secondNumber = 2.222;
+let thirdNumber = 30;
+forthNumber = 3.333;
+// ONCLICK INPUT PROMPTS USER FOR NUMBER AND DISPLAYS
+function input() {
+  let number = prompt('Enter a number');
+  countEl.textContent = number;
+  let meterNumber = number;
+  let meterConversion = 3.28084;
+  meterNumber *= meterConversion;
+  let feetNumber = number;
+  let feetConversion = 0.3048;
+  feetNumber *= feetConversion;
+  firstP.textContent =
+    number +
+    ' meters = ' +
+    meterNumber.toFixed(3) +
+    ' feet | ' +
+    feetNumber +
+    ' feet = ' +
+    feetConversion.toFixed(3) +
+    ' meters';
+}
+
+// .toFixed(3) tells javascript to stop at the 3rd decimal point
