e00683938dcd7b04e153d61bd658f5c9436d9736
Nelly <NellyisDev@gmail.com>
2022-05-26 11:55:53 -0400

    fixing file directories to make CSS work correctly

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -5,9 +5,9 @@ let icon = document.getElementById('icon');
 icon.onclick = () => {
   document.body.classList.toggle('dark-theme');
   if (document.body.classList.contains('dark-theme')) {
-    icon.src = '../src/img/moon.png';
+    icon.src = './img/moon.png';
   } else {
-    icon.src = '../src/img/sun.png';
+    icon.src = './img/sun.png';
   }
 };
 
